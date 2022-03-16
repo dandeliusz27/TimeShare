6ef2e77a04dd61de1db6e7fd1a32bd577d01d2db
dandeliusz27 <dandeliusz27@gmail.com>
2022-03-16 16:45:42 +0100

    profile page changes

--- src/routes/ProfilePage.jsx
+++ src/routes/ProfilePage.jsx
@@ -1,16 +1,22 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import React from 'react'
-import LogoutButton from '../components/LogoutButton'
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+import LogoutButton from '../components/LogoutButton';
+import './ProfilePage.css';
 
 function ProfilePage() {
   const {user} = useAuth0();
   return (
-    <div>
+    <div className='profilePageContainer'>
+        <img src={user.picture} alt="profile_picture" />
+        <div>given name: {user.given_name}</div>
+        <div>nickname: {user.nickname}</div>
+        <div>name: {user.name}</div>
+        <div>locale: {user.locale}</div>
+        <div>updated at:{user.updated_at}</div>
+        <div>email: {user.email}</div>
+        <div>email verified: {user.email_verified}</div>
+        <div>sub: {user.sub}</div>
         <LogoutButton/>
-        <div>
-          {JSON.stringify(user, null, 2)}
-        </div>
-        
     </div>
   )
 }
